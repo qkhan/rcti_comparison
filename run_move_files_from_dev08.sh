8be2d900be4863aa48df54516e1297b5a18b12d6
qkhan <qaisarjkhan@gmail.com>
2020-09-17 19:06:32 +1000

    updated

--- run_move_files_from_dev08.sh
+++ run_move_files_from_dev08.sh
@@ -1,2 +1,2 @@
 source ../menv/bin/activate
-/Users/qaisar/Desktop/rcti_comparison_scripts/commission-comparer-infynity/move_files_from_dev08.sh 1 12638 23564_Thu_Sep_17_2020
+/Users/qaisar/Desktop/rcti_comparison_scripts/commission-comparer-infynity/move_files_from_dev08.sh 1 21516 23759_Thu_Sep_17_2020
